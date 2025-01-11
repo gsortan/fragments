53f2e9e5c5493ac919704d35041bb4a334f517f3
Gordon Tan <gordontan.dev@projecthumancity.com>
2025-01-11 02:38:43 -0500

    Adding printing of env variables in logger with debug log level

--- src/logger.js
+++ src/logger.js
@@ -5,6 +5,7 @@ const options = { level: process.env.LOG_LEVEL || 'info' };
 
 // If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
+  console.log(process.env);
   options.transport = {
     target: 'pino-pretty',
     options: {
